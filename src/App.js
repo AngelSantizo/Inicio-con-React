3332515dfb479cacf61c8b90b579e3b33c866eb4
Angel Santizo <aangelsanti12@gmail.com>
2024-04-17 10:57:22 -0600

    Agregando SASS al proyecto, modificando archivos css a scss y agregando diseño

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Item from './componentes/item/item.js';
+import Menu from './componentes/menu/menu.js';
 
 function App() {
   return (
     <div className="App">
+      <Menu></Menu>
       <Item></Item>
     </div>
   );
--- src/componentes/item/item.js
+++ src/componentes/item/item.js
@@ -1,17 +1,21 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+//tenemos que importar tambien la hoja de estilos .scss
+import './item.scss'
 
 function Item() {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        <Card.Title>Realizar proyecto</Card.Title>
+        <Card.Text className='fw-bold'>
+          Descripcion
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Card.Text>
+          Elaborar proyecto del curso de desarrollo web por Angel Santizo
+        </Card.Text>
+        <Button variant="primary">Remove</Button>
       </Card.Body>
     </Card>
   );
