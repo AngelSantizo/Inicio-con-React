9c1f685610af93d00486a17e55fc780f05c912f1
Angel Santizo <aangelsanti12@gmail.com>
2024-04-19 11:38:01 -0600

    Agregando el formulario

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Item from './componentes/item/item.js';
 import Menu from './componentes/menu/menu.js';
+import Formulario from './componentes/form/form.js';
 
 function App() {
   return (
     <div className="App">
       <Menu></Menu>
+      <Formulario></Formulario>
       <Item></Item>
     </div>
   );
