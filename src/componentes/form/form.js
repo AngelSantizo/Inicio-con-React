ccf5a88390b4d7f13e5e371fd8be68d0107bcfcf
Angel Santizo <aangelsanti12@gmail.com>
2024-04-24 17:04:13 -0600

    agregando el evento en el formulario e instalacion de react-redux

--- src/componentes/form/form.js
+++ src/componentes/form/form.js
@@ -2,22 +2,47 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 //agregamos .scss
 import './form.scss'
+//agregamos dispatch para hacer uso de eventos con reux
+import { useDispatch } from 'react-redux';
+
+//importamos la accion osea el reducer
+import {
+  addGoal
+} from '../../reducers/goals';
+
+//importaremos useRef para obtener la informacion del formulario (esto ya es de react)
+import { useRef } from 'react';
 
 function Formulario() {
+  //crearemos variables para obtener la informacion del formulario
+  const inputRefName = useRef();
+  const inputRefDes = useRef();
+  const inputRefDate = useRef();
+
+  //ahora haremos la funcion para agregar la meta en el item
+  const addItem = (e)=>{
+    e.preventDefault();
+    useDispatch(addGoal({
+      name:inputRefName.current.value,
+      description: inputRefDes.current.value,
+      dueDate: inputRefDate.current.value
+    }));
+  }
+
   return (
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label className='FormLabel'>Nombre</Form.Label>
-        <Form.Control type="text" />
+        <Form.Control type="text" ref={inputRefName}/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label className='FormLabel'>Descripcion</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+        <Form.Control as="textarea" rows={3} ref={inputRefDes}/>
       </Form.Group>
       <Form.Label className='FormLabel'>Deadline</Form.Label>
-      <Form.Control type="date" />
-      <Button type="submit" className='FormBoton'>
+      <Form.Control type="date" ref={inputRefDate}/>
+      <Button type="submit" className='FormBoton' onClick={addItem}>
         Submit
       </Button>
     </Form>
