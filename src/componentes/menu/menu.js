560b46210873f63a90489f51882c13979dcf383e
Angel Santizo <aangelsanti12@gmail.com>
2024-05-11 13:54:23 -0600

    cambios en diseño

--- src/componentes/menu/menu.js
+++ src/componentes/menu/menu.js
@@ -9,23 +9,12 @@ function Menu() {
   return (
     <Navbar expand="lg" className="NavBar">
       <Container>
-      <Navbar.Brand className='MenuBrand'>Tareas</Navbar.Brand>
+      <Navbar.Brand className='MenuBrand'>ToDo</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home" className='HomeText'>Home</Nav.Link>
-            <Nav.Link href="#link" className='LinkText'>Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#home" className='HomeText'>Tasks</Nav.Link>
+            <Nav.Link href="#link" className='LinkText'>Goals</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
